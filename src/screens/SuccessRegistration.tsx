70ffe8421cda5289462e7c6302eecf1ad9a749e9
Lau Xin Wei <xinweii1996@live.com>
2023-06-18 14:11:36 +0800

    feat: use alternative component UI library

--- src/screens/SuccessRegistration.tsx
+++ src/screens/SuccessRegistration.tsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { Image } from 'expo-image';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Button } from '@rneui/themed';
+import { Button } from 'react-native-paper';
 import tw from 'twrnc'
 
 // Use require to import the SVG file
@@ -23,18 +23,9 @@ export default function SuccessRegistration() {
             </View>
 
             <View style={tw`flex items-center justify-center h-1/5`}>
-                <Button
-                    ViewComponent={LinearGradient} // Don't forget this!
-                    style={tw` w-full `}
-                    containerStyle={tw`w-full rounded-full`}
-                    linearGradientProps={{
-                        colors: ['#8658E8', '#9DCEFF'],
-                        start: { x: 0.2, y: 0.5 },
-                        end: { x: 1, y: 0.5 },
-                    }}
-                >
-                    Go To Home
-                </Button>
+                <LinearGradient style={tw`rounded-full w-full shadow-lg`} colors={['#9DCEFF', '#8658E8']} locations={[0.7, 0]} start={{ x: 0.2, y: 0.5 }} end={{ x: 1, y: 0.5 }}>
+                    <Button mode="text" style={tw`w-full shadow-lg `} textColor='white'>Go To Home</Button>
+                </LinearGradient>
             </View>
             <StatusBar style="auto" />
         </View >
