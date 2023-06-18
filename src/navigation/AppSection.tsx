5f7a3300468e36793c0c93bb155915d6168ccfbb
Lau Xin Wei <xinweii1996@live.com>
2023-06-18 21:42:12 +0800

    feat: add sign up screen

--- src/navigation/AppSection.tsx
+++ src/navigation/AppSection.tsx
@@ -1,12 +1,16 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../screens/Login";
+import SignUp from "../screens/SignUp";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppSection() {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator screenOptions={{
+            headerShown: false,
+        }}>
             <Stack.Screen name="login" component={Login} />
-        </Stack.Navigator>
+            <Stack.Screen name="signup" component={SignUp} />
+        </Stack.Navigator >
     )
 }
