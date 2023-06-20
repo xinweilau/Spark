4796bb8f190c32e6b49b578ff899ea091ebc59e3
Lau Xin Wei <xinweii1996@live.com>
2023-06-20 14:33:17 +0800

    refactor: add new properties for Activity

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -20,6 +20,9 @@ const FAKE_ACTIVITIES_DATA: Activity[] = [
         },
         dateTime: new Date(),
         location: 'R1OT',
+        description: 'A spin class is a high-intensity cycling workout that generally takes place on a stationary machine with a heavy, weighted flywheel that is linked to the pedals.',
+        numParticipants: 1,
+        maxParticipants: 10,
     },
     {
         id: '2',
@@ -30,6 +33,9 @@ const FAKE_ACTIVITIES_DATA: Activity[] = [
         },
         dateTime: new Date(),
         location: 'Fitness First Clementi',
+        description: 'Hot yoga is a vigorous form of yoga performed in a studio that is heated to 105 F (40 C) and has a humidity of 40 percent. The formal name for hot yoga is Bikram yoga.',
+        numParticipants: 0,
+        maxParticipants: 10,
     },
     {
         id: '3',
@@ -40,6 +46,9 @@ const FAKE_ACTIVITIES_DATA: Activity[] = [
         },
         dateTime: new Date(),
         location: 'Sungei Gedong Camp',
+        description: 'The Individual Physical Proficiency Test (IPPT) is a standard physical fitness test used by the Singapore Armed Forces (SAF), Singapore Civil Defence Force (SCDF), and Singapore Police Force (SPF) to test the basic components of physical fitness.',
+        numParticipants: 5,
+        maxParticipants: 10,
     }
 ]
 
