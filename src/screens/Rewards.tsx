76d4a488aa86da3c6dfe016afafbddb6caacfbc8
Tan Hu Soon <62252682+husoonnn@users.noreply.github.com>
2023-06-22 02:05:10 +0800

    Rewards

--- src/screens/Rewards.tsx
+++ src/screens/Rewards.tsx
@@ -2,10 +2,13 @@ import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { Image } from "expo-image";
 import { useLayoutEffect, useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import tw from "twrnc"
 import EventDetailBar from "../navigation/EventDetailBar";
 import { Activity } from "../types/Activity";
-
+import { Button, Dialog, PaperProvider, Portal } from "react-native-paper";
+import { Ionicons } from '@expo/vector-icons';
+import tw from "twrnc"
+import { formatDate, formatTime } from "../utils/dateTime";
+import GradientButton from "../components/GradientButton";
 const basketball = require('../../assets/images/basketball.svg');
 
 export default function Rewards() {
@@ -13,13 +16,31 @@ export default function Rewards() {
     const route = useRoute<RouteProp<RewardsScreenProps>>();
     const activity = route.params as Activity;
 
-    
-
     return (
-       <View>
-        <Text>
-            asd
-        </Text>
-       </View>
+        <PaperProvider>
+            <View style={tw`flex flex-col w-full h-full bg-white`}>
+                <View style={tw`w-full flex-1`}>
+                    <Image
+                        source={basketball}
+                        style={tw`w-full h-60 rounded-bl-3xl rounded-br-3xl`}
+                        contentFit="cover" />
+                    <View style={tw`p-5`}>
+                        <Text style={tw`font-semibold`}>
+                            Yearly Leaderboard: 7 days tour to Japan
+                        </Text>
+                    </View>
+                    <Image
+                        source={basketball}
+                        style={tw`w-full h-60 rounded-bl-3xl rounded-br-3xl`}
+                        contentFit="cover" />
+                  
+                    <View style={tw`p-5`}>
+                        <Text style={tw`font-semibold`}>
+                        Monthly Leaderboard: Pair of Tickets to Adventure Cove
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        </PaperProvider>
     )
 }
