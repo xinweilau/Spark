f92df58302ec59dc4baaf1c1c5df731554b5d261
Tan Hu Soon <62252682+husoonnn@users.noreply.github.com>
2023-06-21 11:29:47 +0800

    Settings page

---
+++ src/navigation/Settings.tsx
@@ -0,0 +1,286 @@
+import React from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Switch,
+} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+const avatar = require('../../assets/images/login.svg');
+const SECTIONS = [
+  {
+    header: 'Preferences',
+    icon: 'settings',
+    items: [
+      { label: 'Language', value: 'English', type: 'input' },
+      { label: 'Dark Mode', value: false, type: 'boolean' },
+      { label: 'Use Wi-Fi', value: true, type: 'boolean' },
+      { label: 'Location', value: 'Los Angeles, CA', type: 'input' },
+      { label: 'Show collaborators', value: true, type: 'boolean' },
+      { label: 'Accessibility mode', value: false, type: 'boolean' },
+    ],
+  },
+];
+
+export default function Settings() {
+    const [value, setValue] = React.useState(0);
+    const { tabs, items } = React.useMemo(() => {
+      return {
+        tabs: SECTIONS.map(({ header, icon }) => ({
+          name: header,
+          icon,
+        })),
+        items: SECTIONS[value].items,
+      };
+    }, [value]);
+
+    return (
+      <SafeAreaView style={{ backgroundColor: '#f8f8f8', flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.title}>Settings</Text>
+
+          <Text style={styles.subtitle}>
+            Lorem ipsum dolor sit amet consectetur.
+          </Text>
+        </View>
+
+        <View style={styles.profile}>
+          <View style={styles.profileHeader}>
+            <Image
+              alt=""
+              source={{
+                uri: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80',
+              }}
+              style={styles.profileAvatar}
+            />
+
+            <View style={styles.profile}>
+              <Text style={styles.profileName}>John Doe</Text>
+              <Text style={styles.profileHandle}>@john.doe</Text>
+            </View>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => {
+              // handle onPress
+            }}>
+            <View style={styles.profileAction}>
+              <Text style={styles.profileActionText}>Edit Profile</Text>
+
+              <FeatherIcon color="#fff" name="edit-3" size={16} />
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.content}>
+          <View style={styles.tabs}>
+            {tabs.map(({ name, icon }, index) => {
+              const isActive = index === value;
+
+              return (
+                <View
+                  key={name}
+                  style={[
+                    styles.tabWrapper,
+                    isActive && { borderBottomColor: '#6366f1' },
+                  ]}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      setValue(index);
+                    }}>
+                    <View style={styles.tab}>
+                      <FeatherIcon
+                        color={isActive ? '#6366f1' : '#6b7280'}
+                        name={icon}
+                        size={16}
+                      />
+
+                      <Text
+                        style={[
+                          styles.tabText,
+                          isActive && { color: '#6366f1' },
+                        ]}>
+                        {name}
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              );
+            })}
+          </View>
+
+          {items.map(({ label, type, value }, index) => {
+            return (
+              <View
+                key={label}
+                style={[
+                  styles.rowWrapper,
+                  index === 0 && { borderTopWidth: 0 },
+                ]}>
+                <TouchableOpacity
+                  onPress={() => {
+                    // handle onPress
+                  }}>
+                  <View style={styles.row}>
+                    <Text style={styles.rowLabel}>{label}</Text>
+
+                    <View style={styles.rowSpacer} />
+
+                    {type === 'input' && (
+                      <Text style={styles.rowValue}>{value}</Text>
+                    )}
+
+                  
+
+                    {(type === 'input' || type === 'link') && (
+                      <FeatherIcon
+                        color="#7f7f7f"
+                        name="chevron-right"
+                        size={20}
+                      />
+                    )}
+                  </View>
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    paddingLeft: 24,
+    paddingRight: 24,
+    marginBottom: 12,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#1d1d1d',
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#929292',
+  },
+  profile: {
+    paddingTop: 12,
+    paddingHorizontal: 24,
+    paddingBottom: 24,
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#e3e3e3',
+  },
+  profileHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  profileAvatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 9999,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginRight: 12,
+  },
+  profileName: {
+    fontSize: 17,
+    fontWeight: '600',
+    color: '#3d3d3d',
+  },
+  profileHandle: {
+    marginTop: 4,
+    fontSize: 15,
+    color: '#989898',
+  },
+  profileAction: {
+    marginTop: 16,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#007bff',
+    borderRadius: 12,
+  },
+  profileActionText: {
+    marginRight: 8,
+    fontSize: 15,
+    fontWeight: '600',
+    color: '#fff',
+  },
+  tabs: {
+    padding: 16,
+    flexDirection: 'row',
+  },
+  tab: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  tabWrapper: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    borderColor: '#e5e7eb',
+    borderBottomWidth: 2,
+  },
+  tabText: {
+    fontSize: 13,
+    fontWeight: '600',
+    color: '#6b7280',
+    marginLeft: 5,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 50,
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  rowWrapper: {
+    borderTopWidth: 1,
+    borderColor: '#e3e3e3',
+  },
+  rowLabel: {
+    fontSize: 17,
+    fontWeight: '500',
+    color: '#2c2c2c',
+  },
+  rowValue: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: '#7f7f7f',
+    marginRight: 4,
+  },
+  rowSpacer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  container: {
+    paddingVertical: 24,
+  },
+  content: {
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderColor: '#e3e3e3',
+  },
+});
