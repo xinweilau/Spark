0b6b383eafe6c049dae1beb76bf7a893b65f833c
Lau Xin Wei <xinweii1996@live.com>
2023-06-22 22:44:30 +0800

    feat: integrate calendar component with backend

--- src/screens/Calendar.tsx
+++ src/screens/Calendar.tsx
@@ -1,67 +1,80 @@
-import React, {useState} from 'react';
-import {View, TouchableOpacity, TouchableWithoutFeedback, ScrollView, Text, StyleSheet, SafeAreaView} from 'react-native';
-import {Agenda} from 'react-native-calendars';
-import {Card, Avatar} from 'react-native-paper';
+import React, { useEffect, useState } from 'react';
+import { View, TouchableOpacity, TouchableWithoutFeedback, ScrollView, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { Agenda } from 'react-native-calendars';
+import { Card, Avatar } from 'react-native-paper';
 import tw from 'twrnc';
 import Tag from "../components/Tag";
 import { Ionicons } from '@expo/vector-icons';
 import { Activity } from '../types/Activity';
 import { ACTIVITY_DATA } from '../utils/mock';
 import ActivityItem from '../components/ActivityItem';
+import { getUserRegisteredActivity } from '../services/User';
+import { useQuery } from '@tanstack/react-query';
 
 const timeToString = (time: any) => {
   const date = new Date(time);
   return date.toISOString().split('T')[0];
 };
 
-export default function Calendar(){
+export default function Calendar() {
   const [items, setItems] = useState({});
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    if (activities.length > 0) {
+      const items: any = {}
+      activities.forEach((activity: Activity) => {
+        const date = timeToString(activity.startTime)
+        if (items[date] === undefined) {
+          items[date] = []
+        }
+        items[date].push(activity)
+      })
+
+      setItems(items)
+    }
+  }, [activities])
+
+  const formatDate = (date: Date) => {
+    const formattedDate = date.toLocaleString('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+      weekday: 'long',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    });
+
+    return formattedDate
+  }
+
+
+  const { refetch } = useQuery({
+    queryKey: ['registeredActivities', 1],
+    queryFn: () => getUserRegisteredActivity("1"),
+    onSuccess: ({ data }: any) => {
+      const registeredActivities: Activity[] = data.result.map((data: any): Activity => {
+        return {
+          id: data.id,
+          title: data.title,
+          description: data.description,
+          category: data.category,
+          subCategory: data.subcategory,
+          location: data.location,
+          startTime: new Date(data.starttime),
+          endTime: new Date(data.endtime),
+          maxParticipants: data.maxparticipants,
+        }
+      })
+
+      setActivities(registeredActivities)
+    },
+    retry: false,
+  })
 
   const loadItems = (day: any) => {
-    setTimeout(() => {
-    
-    const items:any = {
-        "2023-06-21":[
-            {id: '1',
-            title: 'Spin Class',
-            category: 'Fitness',
-            dateTime: "2023-06-21",
-            location: 'R1OT',
-            description: 'A spin class is a high-intensity cycling workout that generally takes place on a stationary machine with a heavy, weighted flywheel that is linked to the pedals.',
-            numParticipants: 1,
-            maxParticipants: 10,}
-           ,{id: '2',
-           title: 'Hot Yoga',
-           category: 'Fitness',
-           dateTime: "2023-06-21",
-           location: 'Fitness First Clementi',
-           description: 'Hot yoga is a vigorous form of yoga performed in a studio that is heated to 105 F (40 C) and has a humidity of 40 percent. The formal name for hot yoga is Bikram yoga.',
-           numParticipants: 0,
-           maxParticipants: 10,}
-            ],
-        "2023-06-23":[
-            {id: '3',
-            title: 'IPPT Training',
-            category: "Fitness",
-            dateTime: "2023-06-23",
-            location: 'Sungei Gedong Camp',
-            description: 'The Individual Physical Proficiency Test (IPPT) is a standard physical fitness test used by the Singapore Armed Forces (SAF), Singapore Civil Defence Force (SCDF), and Singapore Police Force (SPF) to test the basic components of physical fitness.',
-            numParticipants: 5,
-            maxParticipants: 10,}
-        ]
-    }
-
-
-
-      const newItems:any = {};
-      Object.keys(items).forEach((key) => {
-        if (typeof key === "string"){
-            console.log('string')
-        }
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
+    refetch();
   };
 
   const renderItem = (item: any) => {
@@ -77,58 +90,57 @@ export default function Calendar(){
     // );
     return (
 
-    // <TouchableWithoutFeedback onPress={handlePress}>
-    <View style={tw`pb-2 py-3`}>
+      // <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={tw`pb-2 py-3`}>
         <TouchableWithoutFeedback>
-            <View style={tw`flex flex-col gap-8 rounded-3xl bg-[#F0F0FF] p-5 justify-between shadow-sm`}>
-                <View style={tw`flex flex-col gap-2`}>
-                    <View style={tw`flex flex-row flex-nowrap items-center justify-between`}>
-                        <Text style={tw`text-base font-semibold`}>{item.title}</Text>
-                        <Tag>{item.category}</Tag>
-                    </View>
+          <View style={tw`flex flex-col gap-8 rounded-3xl bg-[#F0F0FF] p-5 justify-between shadow-sm`}>
+            <View style={tw`flex flex-col gap-2`}>
+              <View style={tw`flex flex-row flex-nowrap items-center justify-between`}>
+                <Text style={tw`text-base font-semibold`}>{item.title}</Text>
+                <Tag>{item.category}</Tag>
+              </View>
 
-                    <Text style={tw`text-xs text-[#404446]`}>
-                        {item.dateTime}
-                    </Text>
-                </View>
-
-                <View style={tw`flex flex-row items-center gap-1`}>
-                    <Ionicons name="location" size={18} color='#404446' />
-                    <Text style={tw`text-xs text-[#404446]`}>{item.location}</Text>
-                </View>
+              <Text style={tw`text-xs text-[#404446]`}>
+                {formatDate(item.startTime)}
+              </Text>
             </View>
+
+            <View style={tw`flex flex-row items-center gap-1`}>
+              <Ionicons name="location" size={18} color='#404446' />
+              <Text style={tw`text-xs text-[#404446]`}>{item.location}</Text>
+            </View>
+          </View>
         </TouchableWithoutFeedback>
-    </View>
+      </View>
 
     )
   };
 
   return (
-    <View style={{flex: 1, paddingTop: 34,}}>
-    <Agenda
-      items={items}
-      loadItemsForMonth={loadItems}
-      renderItem={renderItem}
-    />
-  </View>
+    <View style={{ flex: 1, paddingTop: 34, }}>
+      <Agenda
+        items={items}
+        loadItemsForMonth={loadItems}
+        renderItem={renderItem}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
 
-    item: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      backgroundColor: 'white',
-      borderRadius: 5,
-      padding: 10,
-      marginRight: 10,
-      marginTop: 10,
-    },
-    emptyDate: {
-      height: 15,
-      flex: 1,
-      paddingTop: 30,
-    },
-  });
-  
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 10,
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30,
+  },
+});
