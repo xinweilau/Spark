fcef730a5a7a5b46990a77584123a177ffc06ea4
Lau Xin Wei <xinweii1996@live.com>
2023-06-22 22:25:41 +0800

    feat: disable log messages for demonstration in dev mode

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { AuthProvider } from "./src/contexts/AuthContext";
 import Router from "./src/navigation/Router";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { LogBox } from 'react-native';
+
+LogBox.ignoreAllLogs(true);
 
 const queryClient = new QueryClient();
 
